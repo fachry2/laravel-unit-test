4e0c6b4f902430999e4d51e00cadfa828c4901fd
fachry2 <hckraza@gmail.com>
2022-07-03 12:32:43 +0700

    fix controller test

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use App\Book;
 use App\Http\Requests\PostBookRequest;
 use App\Http\Resources\BookResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 
 class BooksController extends Controller
 {
@@ -17,14 +20,54 @@ class BooksController extends Controller
     public function index(Request $request)
     {
         // @TODO implement
-        
-        return BookResource::collection();
+        $direction = 'ASC';
+        $book      = Book::query();
+        if ($request->has('title')) {
+            $book->where('title', 'like', "%$request->title%");
+        }
+        if ($request->has('authors')) {
+            $book->whereHas('authors', function (Builder $query) use ($request) {
+                $query->whereIn('id', explode(',', $request->authors));
+            });
+        }
+        if ($request->has('sortColumn')) {
+            if ($request->has('sortDirection')) {
+                $direction = $request->sortDirection;
+            }
+            if ($request->sortColumn !== 'avg_review') {
+                $book->orderBy($request->sortColumn, $direction);
+            } else {
+            }
+        }
+        $books = $book->paginate(15);
+        if ($request->has('sortColumn') && $request->sortColumn === 'avg_review') {
+            if ($direction === 'ASC') {
+                $books = $books->sortBy(function ($query) {
+                    return $query->reviews->avg('review');
+                })->all();
+            } else {
+                $books = $books->sortByDesc(function ($query) {
+                    return $query->reviews->avg('review');
+                })->all();
+            }
+        }
+
+        return BookResource::collection($books);
     }
 
     public function store(PostBookRequest $request)
     {
         // @TODO implement
+        $author               = Author::whereIn('id', $request['authors'])->first();
         $book = new Book();
+        $book->isbn           = $request['isbn'];
+        $book->title          = $request['title'];
+        $book->description    = $request['description'];
+        $book->published_year = $request['published_year'];
+        $book->save();
+        $book->authors()->saveMany([
+            $author,
+        ]);
 
         return new BookResource($book);
     }
--- app/Http/Controllers/BooksReviewController.php
+++ app/Http/Controllers/BooksReviewController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use App\BookReview;
 use App\Http\Requests\PostBookReviewRequest;
 use App\Http\Resources\BookReviewResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BooksReviewController extends Controller
 {
@@ -17,7 +19,13 @@ class BooksReviewController extends Controller
     public function store(int $bookId, PostBookReviewRequest $request)
     {
         // @TODO implement
+        $book = Book::findOrFail($bookId);
         $bookReview = new BookReview();
+        $bookReview->review = $request['review'];
+        $bookReview->comment = $request['comment'];
+        $bookReview->user()->associate(Auth::user());
+        $bookReview->book()->associate($book);
+        $bookReview->save();
 
         return new BookReviewResource($bookReview);
     }
@@ -25,5 +33,8 @@ class BooksReviewController extends Controller
     public function destroy(int $bookId, int $reviewId, Request $request)
     {
         // @TODO implement
+        $book = Book::findOrFail($bookId);
+        BookReview::destroy($reviewId);
+        return response()->noContent();
     }
 }
