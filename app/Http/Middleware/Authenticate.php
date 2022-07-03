231fd522d48cf73c344b35a5f7accaa39bd29121
fachry2 <hckraza@gmail.com>
2022-07-03 12:27:16 +0700

    fix auth for acting user and middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -18,4 +18,31 @@ class Authenticate extends Middleware
             return route('login');
         }
     }
+
+    /**
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+        $token = $request->bearerToken();
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+        if ($this->auth->guard('web')->check()) {
+            return $this->auth->shouldUse('web');
+        }
+
+        $this->unauthenticated($request, $guards);
+    }
 }
